a8b707d731f9be9449f3ae8cc3f2c20f59e62977
LAPTOP-L9JA9ATK\580da <22aj050@ms.dendai.ac.jp>
2025-01-31 04:52:36 +0900

    TeacherMain.javaを追加

---
+++ TeacherMain.java
@@ -0,0 +1,6 @@
+public class TeacherMain {
+    public static void main(String[] args) {
+        Teacher teacher = new Teacher("00ta000", "Taro");
+        System.out.println(teacher.getId() + " " + teacher.getName());
+    }
+}
