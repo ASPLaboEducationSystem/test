e3e2302e102944708700ff0dfac3e551ac8ce2ff
LAPTOP-L9JA9ATK\580da <22aj050@ms.dendai.ac.jp>
2025-01-31 03:50:50 +0900

    A1_1.javaの追加

---
+++ A1_1.java
@@ -0,0 +1,6 @@
+public class A1_1 {
+    public static void main(String[] args) {
+        Student student1 = new Student("00aj000", "Taro");
+        System.out.println(student1.getId() + " " + student1.getName());
+    }
+}
